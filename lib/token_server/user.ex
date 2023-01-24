690ee453536b8e55f40159d3363085b2059a05ed
Raul André <raulandre.dev@gmail.com>
2023-01-24 20:04:58 -0300

    Fix formatting

--- lib/token_server/user.ex
+++ lib/token_server/user.ex
@@ -19,7 +19,9 @@ defmodule TokenServer.User do
     |> put_password_hash()
   end
 
-  defp put_password_hash(%Ecto.Changeset{valid?: true, changes: %{password: password}} = changeset) do
+  defp put_password_hash(
+         %Ecto.Changeset{valid?: true, changes: %{password: password}} = changeset
+       ) do
     change(changeset, Argon2.add_hash(password))
   end
 
