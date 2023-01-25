a5068578a2685e01d6a70f1eeaa4fe192e6e4332
Raul André <raulandre.dev@gmail.com>
2023-01-24 21:35:48 -0300

    Fix tests

--- test/token_server_web/views/error_view_test.exs
+++ test/token_server_web/views/error_view_test.exs
@@ -1,12 +1,9 @@
 defmodule TokenServerWeb.ErrorViewTest do
   use TokenServerWeb.ConnCase, async: true
+  alias TokenServer.User
 
   # Bring render/3 and render_to_string/3 for testing custom views
   import Phoenix.View
-
-  test "renders 404.json" do
-    assert render(TokenServerWeb.ErrorView, "404.json", []) == %{errors: %{detail: "Not Found"}}
-  end
 
   test "renders 500.json" do
     assert render(TokenServerWeb.ErrorView, "500.json", []) ==
