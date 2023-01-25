e537e1df7952ceac77fd08f1d967bcb701993da3
Raul André <raulandre.dev@gmail.com>
2023-01-24 21:37:25 -0300

    Fix tests

--- test/token_server_web/views/error_view_test.exs
+++ test/token_server_web/views/error_view_test.exs
@@ -4,9 +4,4 @@ defmodule TokenServerWeb.ErrorViewTest do
 
   # Bring render/3 and render_to_string/3 for testing custom views
   import Phoenix.View
-
-  test "renders 500.json" do
-    assert render(TokenServerWeb.ErrorView, "500.json", []) ==
-             %{errors: %{detail: "Internal Server Error"}}
-  end
 end
